c4a4ff4b020e8ef6ec5b693582464e333ec6b3f1
BQ <berend.quist@gmail.com>
2023-08-10 21:52:24 +0200

    Added docstrings to model and added prevSuborderCompleted.

--- production_optimisation/problem_declaration/modeltry.py
+++ production_optimisation/problem_declaration/modeltry.py
@@ -23,6 +23,7 @@ class EWOptimisation:
 
         # Retrieving data
         order_suborder = self.data_index.get_index_set('order_suborder')
+        suborder_set = self.data_index.get_index_set('suborder')
         time = self.data_index.get_index_set('time')
         employee_line = self.data_index.get_index_set('employee_line')
         employee = self.data_index.get_index_set('employee')
@@ -54,7 +55,6 @@ class EWOptimisation:
         # Create variables
         self.m.var_alloc = pyo.Var(self.m.set_alloc_index, domain=pyo.Binary)
 
-        # total planned hours per order rule
         
         # Create objective
 
@@ -70,7 +70,7 @@ class EWOptimisation:
                 i (str): Order index
 
             Returns:
-                Expression: Used in pyo.Constraint as rule. 
+                Expression: time_req_lb <= sum over time and employees and or lines for allocation <= req_time_up
             """
             return (
                 time_req_lb.loc[i],
@@ -83,7 +83,18 @@ class EWOptimisation:
             )
         self.m.constr_required_planned_hours = pyo.Constraint(self.m.set_order_suborder, rule=rule_requiredPlannedHours)
 
+
         def rule_oneAllocPerEmpl_linePerMoment(m, j, k):
+            """This rule makes sure that at any given moment every employee and/or line {k} is allocated a maximum of one time.
+
+            Args:
+                m (Model): Pyomo model
+                j (Time): Time interval
+                k (empl_line): Employees and/or Lines
+
+            Returns:
+                Expression: 0<= sum over order_suborder's for allocation <= 1
+            """
             return (
                 0, 
                 sum(m.var_alloc[(i, j, k)]
@@ -93,20 +104,78 @@ class EWOptimisation:
             )
         self.m.constr_oneAllocPerEmpl_linPerMoment = pyo.Constraint(self.m.set_time, self.m.set_employee_line, rule=rule_oneAllocPerEmpl_linePerMoment)
 
+
         def rule_onlyAllocIfEmpl_lineSkilled(m, i, j, k):
+            """This rule makes sure that an employee and or line can only be allocated to a suborder if it is skilled to do so.
+
+            Args:
+                m (Model): Pyomo Model
+                i (order_suborder): Order_suborder
+                j (Time): Time intervals
+                k (empl_line): Employees and/or Lines
+
+            Returns:
+                Expression: 0 <= allocation <= 1 (skills_df has value of either 0 or 1)
+            """
             suborder = specific_order_suborder.loc[i].iloc[1]
             return (0, 
                     m.var_alloc[(i, j, k)], 
                     skills_df.loc[k, suborder]) #FIXME: During cleaning every none value should be changed to a 0 value. 
         self.m.constr_onlyAllocIfEmpl_lineSkilled = pyo.Constraint(self.m.set_order_suborder, self.m.set_time, self.m.set_employee_line, rule= rule_onlyAllocIfEmpl_lineSkilled)
     
+
         def rule_onlyAllocIfEmpl_lineAvailable(m, i, j, k):
+            """This rule makes sure that an employee and or line can only be allocated on a time interval if they are available to do so.
+
+            Args:
+                m (Model): Pyomo Model
+                i (order_suborder): Order_suborder
+                j (Time): Time intervals
+                k (empl_line): Employees and/or Lines
+
+            Returns:
+                Expression: 0 <= allocation <= 1 (availability_df has value of either 0 or 1)
+            """
             return (0, 
                     m.var_alloc[(i, j, k)], 
                     availability_df.loc[j, k]) #FIXME: During cleaning every none value should be changed to a 0 value. 
         self.m.constr_onlyAllocIfEmpl_lineAvailable = pyo.Constraint(self.m.set_order_suborder, self.m.set_time, self.m.set_employee_line, rule= rule_onlyAllocIfEmpl_lineAvailable)
     
 
+        def rule_prevSuborderCompletedBeforeNext(m, i, j, k):
+            """This rule makes sure that before the next suborder is allocated, the previous suborder has to be completed.
+
+            Args:
+                m (Model): Pyomo Model
+                i (order_suborder): Order_suborder
+                j (Time): Time intervals
+                k (empl_line): Employees and/or Lines
+
+            Returns:
+                Expression: 0 <= allocation <= a, where a >= 0 and a>= 1 if enough hours of the previous suborder are completed. 
+            """
+            suborder = specific_order_suborder.loc[i].iloc[1]
+            prev_suborder_index = suborder.index(suborder) - 1
+
+            sum_suborders = 0
+            for subord in suborder_set:
+                if suborder_set.index(subord) == prev_suborder_index:
+                    sum_suborder += 1
+            
+            ratio_completedHours_prevSuborders = sum(
+                m.var_alloc[(ord_subord_i, ti_i, empl_line_i)]/time_req_lb.loc[ord_subord_i] #FIXME: PrevPercentage: Add the percentage of previous orders that need to be completed.
+                for ord_subord_i in m.set_order_suborder 
+                for ti_i in m.set_time 
+                for empl_line_i in m.set_employee_line
+            ) #TODO possibly something wrong with the division. See old code.
+
+            if sum_suborders != 0:
+                value = ratio_completedHours_prevSuborders / sum_suborders
+            else:
+                value = ratio_completedHours_prevSuborders
+
+            return (0, m.var_alloc[i,j,k], value)
+        self.m.constr_prevSuborderCompletedBeforeNext = pyo.Constraint(self.m.set_order_suborder, self.m.set_time, self.m.set_employee_line, rule=rule_prevSuborderCompletedBeforeNext)
 
     def solve(self):
         solver = pyomo.opt.SolverFactory('cbc')
