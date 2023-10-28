ce4f204d8094002ca73eeaa6678b45209be732b5
BQ <berend.quist@gmail.com>
2023-10-28 15:01:05 +0200

    ,

--- production_optimisation/main.py
+++ production_optimisation/main.py
@@ -47,7 +47,7 @@ gantt_chart.create_ganttchart()
 gantt_chart.show_plt()
 
 #FIXME: ADD THE CORRECT OBJECTIVE FUNCTION: MINIMIZE GAPS ETC. FIXME
-
+#FIXME: How to add dynamic programming. First solve the schedule, then the allocation of people? max schedule per hour is max free people per hours (dynamicly calculated)
 
 
 #TODO: From the erp system there is a solution for the planning, this can be used as a 'starting point' for solving. 
